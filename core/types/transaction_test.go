60d51554a3139828b642bda5a889288752f1a470
kyberliem <cyberliem.civil@gmail.com>
2020-07-17 18:05:27 +0700

    fmted

--- core/types/transaction_test.go
+++ core/types/transaction_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/Evrynetlabs/evrynet-node/common"
 	"github.com/Evrynetlabs/evrynet-node/crypto"
 	"github.com/Evrynetlabs/evrynet-node/rlp"
-
 )
 
 // The values in those tests are from the Transaction Tests
@@ -55,7 +54,7 @@ var (
 )
 
 func TestTransactionCompatiblity(t *testing.T) {
-	etherHashString:="0x32d0ec31372e18e22e4af25a00926e344c16473978dce150112a89d0bdf5795a"
+	etherHashString := "0x32d0ec31372e18e22e4af25a00926e344c16473978dce150112a89d0bdf5795a"
 	evrTx := NewTransaction(0, common.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87"), big.NewInt(1000), 0, big.NewInt(100), common.FromHex("123"))
 	evrHash := evrTx.Hash()
 
